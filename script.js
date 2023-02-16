595a817f841c4c9b27b792c0933c4395a06699fd
Kristjan Lääne <xonproject@gmail.com>
2023-02-16 18:13:27 +0200

    made a javascript game

---
+++ script.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const displayMessage = message => {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  document.querySelector('.again').addEventListener('click', function () {
+    displayMessage('Start guessing...');
+    score = 20;
+    const secretNumber = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.number').style.width = '15rem';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.guess').value = '';
+  });
+
+  // when there is no input
+  if (!guess) {
+    displayMessage('⛔️ No number!');
+
+    // when player wins
+  } else if (guess === secretNumber) {
+    displayMessage('🎉 Correct Number!');
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+    //when guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '📈 Too High!' : '📉 Too Low!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('😵 You lost the game!');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
